0c7c3cfd97d423ede987a42f6931a847f812e4ae
Simon Kämpe <simon.kampe@gmail.com>
2024-09-13 08:23:47 +0200

    Add stuff

--- zeus/flake.nix
+++ zeus/flake.nix
@@ -80,7 +80,7 @@
           ./rpi4.nix
           ./sd-image.nix
 
-          #common.nixosModules.hardware.argoneon
+          common.nixosModules.hardware.argoneon
 
           common.nixosModules.configuration.common
 
@@ -94,13 +94,13 @@
             # Enable PWM for pwm-fan
             hardware.raspberry-pi."4".pwm0.enable = true;
 
-            #services.argoneon = {
-            #  enable = true;
-            #  package = pkgs.argon;
-            #  withFanControl = false;
-            #  withOledDisplay = true;
-            #  withPowerButton = true;
-            #};
+            services.argoneon = {
+              enable = true;
+              package = pkgs.argon;
+              withFanControl = false;
+              withOledDisplay = true;
+              withPowerButton = true;
+            };
           
             services.tailscale.enable = true;
 
@@ -109,28 +109,28 @@
               openFirewall = true;
             };
 
-            #systemd.services."rpi-pwm-fan" = {
-            #  wantedBy = [ "multi-user.target" ];
-            #  after = [ "sysinit.target" ];
+            systemd.services."rpi-pwm-fan" = {
+              wantedBy = [ "multi-user.target" ];
+              after = [ "sysinit.target" ];
 
-            #  environment = {
-            #    PWM_FAN_BCM_GPIO_PIN_PWM = "18";
-            #    PWM_FAN_PWM_FREQ_HZ = "2500";
-            #    PWM_FAN_MIN_DUTY_CYCLE = "20";
-            #    PWM_FAN_MAX_DUTY_CYCLE = "100";
-            #    PWM_FAN_MIN_OFF_TEMP_C = "38";
-            #    PWM_FAN_MIN_ON_TEMP_C = "40";
-            #    PWM_FAN_MAX_TEMP_C = "46";
-            #    PWM_FAN_FAN_OFF_GRACE_MS = "60000";
-            #    PWM_FAN_SLEEP_MS = "250";
-            #  };
+              environment = {
+                PWM_FAN_BCM_GPIO_PIN_PWM = "18";
+                PWM_FAN_PWM_FREQ_HZ = "2500";
+                PWM_FAN_MIN_DUTY_CYCLE = "20";
+                PWM_FAN_MAX_DUTY_CYCLE = "100";
+                PWM_FAN_MIN_OFF_TEMP_C = "38";
+                PWM_FAN_MIN_ON_TEMP_C = "40";
+                PWM_FAN_MAX_TEMP_C = "46";
+                PWM_FAN_FAN_OFF_GRACE_MS = "60000";
+                PWM_FAN_SLEEP_MS = "250";
+              };
 
-            #  serviceConfig = {
-            #    ExecStart = "${pkgs.raspberry-pi-pwm-fan2}/bin/raspberry-pi-pwm-fan2";
-            #    Restart = "always";
-            #    Type = "simple";
-            #  };
-            #};
+              serviceConfig = {
+                ExecStart = "${pkgs.raspberry-pi-pwm-fan2}/bin/raspberry-pi-pwm-fan2";
+                Restart = "always";
+                Type = "simple";
+              };
+            };
             
             environment.systemPackages = with pkgs; [
               jellyfin
