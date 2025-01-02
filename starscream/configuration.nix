a1cfb5d919152c2b905b8dee707bd2068d46f006
Simon Kämpe <simon.kampe@gmail.com>
2025-01-02 12:20:20 +0100

    Set core affinity

--- starscream/configuration.nix
+++ starscream/configuration.nix
@@ -135,6 +135,11 @@
     };
   };
 
+  # Set systemd affinity to the E-cores
+  systemd.extraConfig = ''
+    CPUAffinity=12 13 14 15 16 17 18 19 20 21
+  '';
+
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
     nerd-fonts.hack
